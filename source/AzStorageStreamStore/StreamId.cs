7e94c03ad657c2d9d49c53c7675a0f286fbd0e78
Richard Bennett <inbox@resurgentpayments.com>
2023-09-29 07:09:33 -0400

    patches equals to handle arrays.

--- source/AzStorageStreamStore/StreamId.cs
+++ source/AzStorageStreamStore/StreamId.cs
@@ -14,6 +14,36 @@ public record StreamId(string TenantId, string[] Hierarchy, string Id) {
         list.Add(id.Id);
         return new(list.ToArray());
     }
+
     public static bool operator ==(StreamId id, StreamKey key) => key.Equals(id);
     public static bool operator !=(StreamId id, StreamKey key) => !key.Equals(id);
+
+    public override int GetHashCode() {
+        var hash = new HashCode();
+        hash.Add(TenantId);
+        foreach (var cat in Hierarchy) {
+            hash.Add(cat.GetHashCode());
+        }
+        hash.Add(Id);
+        return hash.ToHashCode();
+    }
+
+    public virtual bool Equals(StreamId? other) {
+        if (other is null)
+            return false;
+
+        if (ReferenceEquals(this, other)) return true;
+
+        if (!TenantId.Equals(other.TenantId)) return false;
+        if (!Id.Equals(other.Id)) return false;
+
+        if (Hierarchy.Length > other.Hierarchy.Length) return false;
+
+        for (var i = 0; i < Hierarchy.Length; i++) {
+            if (!Hierarchy[i].Equals(other.Hierarchy[i]))
+                return false;
+        }
+
+        return true;
+    }
 }
