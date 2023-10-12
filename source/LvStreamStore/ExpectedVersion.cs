42f70526c380d2ba7b3c507352d5d70a75090cc0
Richard Bennett <richard@resurgentpayments.com>
2023-10-12 12:39:43 -0400

    Adjustments to class to make it a true value object.

--- source/LvStreamStore/ExpectedVersion.cs
+++ source/LvStreamStore/ExpectedVersion.cs
@@ -1,14 +1,31 @@
 namespace LvStreamStore;
 public class ExpectedVersion {
-    public long Value { get; set; }
+    private long _value;
 
-    public static ExpectedVersion EmptyStream = -1;
-    public static ExpectedVersion Any = -2;
-    public static ExpectedVersion NoStream = -3;
+    /// <summary>
+    /// This write should not conflict with anything and should always succeed.
+    /// </summary>
+    public static readonly ExpectedVersion Any = new(-2);
+    /// <summary>
+    /// The stream being written to should not yet exist. If it does exist treat that as a concurrency problem.
+    /// </summary>
+    public static readonly ExpectedVersion NoStream = new(-1);
+    /// <summary>
+    /// The stream should exist and should be empty. If it does not exist or is not empty treat that as a concurrency problem.
+    /// </summary>
+    public static readonly ExpectedVersion EmptyStream = new(-1);
+    /// <summary>
+    /// The stream should exist. If it or a metadata stream does not exist treat that as a concurrency problem.
+    /// </summary>
+    public static readonly ExpectedVersion StreamExists = new(-4);
+
+    public ExpectedVersion(long version) {
+        _value = version;
+    }
 
 
-    public static implicit operator long(ExpectedVersion v) => v.Value;
-    public static implicit operator ExpectedVersion(long value) => new() { Value = value };
-    public override string ToString() => Value.ToString();
-    public override int GetHashCode() => Value.GetHashCode();
+    public static implicit operator long(ExpectedVersion v) => v._value;
+    public static implicit operator ExpectedVersion(long value) => new(value);
+    public override string ToString() => $"ExpectedVersion: {_value}";
+    public override int GetHashCode() => _value.GetHashCode();
 }
