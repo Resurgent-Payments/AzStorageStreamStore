df44e691ae6d31e9e0962982d5e8a7b0843922a4
Richard Bennett <richard@resurgentpayments.com>
2023-11-14 12:33:00 -0500

    Adds the test specification tooling.

--- tests/LvStreamStore.ApplicationToolkit.Tests/ReadModelTests.cs
+++ tests/LvStreamStore.ApplicationToolkit.Tests/ReadModelTests.cs
@@ -1,49 +1,20 @@
 namespace LvStreamStore.ApplicationToolkit.Tests {
     using System;
 
-    using FakeItEasy;
-
-    using LvStreamStore.Serialization.Json;
-
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
+    using LvStreamStore.Test;
 
     using Xunit;
 
-    public partial class ReadModelTests {
-        private readonly MemoryEventStream _eventStream;
-        private readonly ILoggerFactory _loggerFactory;
-        private readonly Dispatcher _bus;
-        private readonly IStreamStoreRepository _repository;
+    public partial class ReadModelTests : StreamStoreTestSpecification {
         private readonly TestReadModel _readModel;
 
-        public ReadModelTests() {
-            _loggerFactory = LoggerFactory.Create((builder) => {
-                builder.AddDebug();
-                builder.SetMinimumLevel(LogLevel.Trace);
-            });
-            _bus = new Dispatcher(_loggerFactory);
-
-            var msOptions = new MemoryEventStreamOptions();
-            var serOptions = new JsonSerializationOptions();
-            var ssRepOptions = new StreamStoreRepositoryOptions();
-
-            var iOptsMsOpts = A.Fake<IOptions<MemoryEventStreamOptions>>();
-            var serOpts = A.Fake<IOptions<JsonSerializationOptions>>();
-            var ssRepOpts = A.Fake<IOptions<StreamStoreRepositoryOptions>>();
-
-            A.CallTo(() => iOptsMsOpts.Value).Returns(msOptions);
-            A.CallTo(() => serOpts.Value).Returns(serOptions);
-            A.CallTo(() => ssRepOpts.Value).Returns(ssRepOptions);
-
-            _eventStream = new MemoryEventStream(_loggerFactory, new JsonEventSerializer(serOpts), iOptsMsOpts);
-            _repository = new StreamStoreRepository(new EmbeddedEventStreamClient(_eventStream), ssRepOpts);
-            _readModel = new TestReadModel(_bus, _repository);
+        public ReadModelTests() : base() {
+            _readModel = new TestReadModel(Bus, Repository);
         }
 
         [Fact]
         public void CanSubscribeToAggregateEvents() {
-            _repository.Save(new TestAggregate(Guid.NewGuid(), "name", "description"));
+            Repository.Save(new TestAggregate(Guid.NewGuid(), "name", "description"));
             AssertEx.IsOrBecomesTrue(() => _readModel.UIModels.Count > 0, TimeSpan.FromSeconds(3));
         }
     }
--- tests/LvStreamStore.ApplicationToolkit.Tests/StreamStoreRepositoryTests.cs
+++ tests/LvStreamStore.ApplicationToolkit.Tests/StreamStoreRepositoryTests.cs
@@ -3,57 +3,28 @@ namespace LvStreamStore.ApplicationToolkit.Tests {
     using System.Linq;
     using System.Threading.Tasks;
 
-    using FakeItEasy;
-
-    using LvStreamStore.Serialization.Json;
-
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
+    using LvStreamStore.Test;
 
     using Xunit;
 
-    public class StreamStoreRepositoryTests : IAsyncLifetime {
-        ILoggerFactory _loggerFactory;
-        IStreamStoreRepository _repository;
-
-        public StreamStoreRepositoryTests() {
-            _loggerFactory = LoggerFactory.Create((builder) => {
-                builder.AddDebug();
-                builder.SetMinimumLevel(LogLevel.Trace);
-            });
-
-            var jsSerOptions = new JsonSerializationOptions();
-            var jsOpts = A.Fake<IOptions<JsonSerializationOptions>>();
-            A.CallTo(() => jsOpts.Value).Returns(jsSerOptions);
-            var jsonEventSerializer = new JsonEventSerializer(jsOpts);
-
-            var msStreamOpts = new MemoryEventStreamOptions();
-            var msOpts = A.Fake<IOptions<MemoryEventStreamOptions>>();
-            A.CallTo(() => msOpts.Value).Returns(msStreamOpts);
-
-            var ssRepOpts = new StreamStoreRepositoryOptions();
-            var ssOpts = A.Fake<IOptions<StreamStoreRepositoryOptions>>();
-            A.CallTo(() => ssOpts.Value).Returns(ssRepOpts);
-
-            var eventStream = new MemoryEventStream(_loggerFactory, jsonEventSerializer, msOpts);
-            var client = new EmbeddedEventStreamClient(eventStream);
-            _repository = new StreamStoreRepository(client, ssOpts);
+    public class StreamStoreRepositoryTests : StreamStoreTestSpecification, IAsyncLifetime {
+        public StreamStoreRepositoryTests() : base() {
         }
 
         public async Task InitializeAsync() {
             var agg = new TestAggregate(Guid.NewGuid(), "name", "description");
-            await _repository.Save(agg);
+            await Repository.Save(agg);
         }
 
         [Fact]
         public async Task ARepositoryCanBeRead() {
-            var reader = _repository.ReadAsync(StreamKey.All);
+            var reader = Repository.ReadAsync(StreamKey.All);
             Assert.Single(await reader.ToListAsync());
         }
 
         [Fact]
         public async Task AggregateEventsCanBeRead() {
-            var reader = _repository.ReadAsync<TestAggregate>();
+            var reader = Repository.ReadAsync<TestAggregate>();
             Assert.Single(await reader.ToListAsync());
         }
 
